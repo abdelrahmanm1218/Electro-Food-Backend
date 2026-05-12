bcb4dc86ffcfb695089e158211d5b15c00049b92
Abdelrahman Mahmoud <abdelrahmannali14@gmail.com>
2026-05-12 11:45:01 +0300

    minor railway configuration edit

--- build.sh
+++ build.sh
@@ -6,12 +6,17 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata menu_items
 
-# Create superuser from environment variables
-python manage.py shell -c "
+python -c "
+import django, os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
-    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
     print('Superuser created.')
 else:
     print('Superuser already exists.')
