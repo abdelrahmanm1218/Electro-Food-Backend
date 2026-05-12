2e98a67b132c701aeaaaf8b3e15184210348f1c5
Abdelrahman Mahmoud <abdelrahmannali14@gmail.com>
2026-05-12 11:32:37 +0300

    adding super user

--- build.sh
+++ build.sh
@@ -5,3 +5,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata menu_items
+
+# Create superuser from environment variables
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
